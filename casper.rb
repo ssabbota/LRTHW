39d4442adf093ab4f39aa49e7dbe1d7713991725
ssabbota <sarah@sabbota.com>
2015-02-02 22:32:39 -0500

    Added lessons

---
+++ casper.rb
@@ -0,0 +1,13 @@
+his_name = 'Casper'
+his_age = 7 # in human years
+his_weight = 40 # lbs
+his_eyes = 'brown'
+his_fur = 'apricot'
+his_activity_level = 'lazy'
+
+puts "Let's talk about #{his_name}."
+puts "He's #{his_age} years old."
+puts "He's #{his_weight} pounds heavy."
+puts "Actually that's pretty heavy."
+puts "He's got #{his_eyes} eyes and #{his_fur} fur."
+puts "He is #{his_activity_level} most of the time."
